c403c2f11bdab506eeb951b936bd1a1a3a32cf75
Miguel Angel Nava Portillo <miguelangel.navaportillo@gmail.com>
2022-09-18 12:58:50 -0500

    Function for showing binary classification

--- Functions/plot_data_binary.py
+++ Functions/plot_data_binary.py
@@ -1,6 +1,7 @@
-def plot_data_binary(X, y):
+def plot_data_binary(X, y,l):
     import matplotlib.pyplot as plt
     plt.scatter(X[y == 0, 0], X[y == 0, 1], label="Class #0", alpha=0.5, linewidth=0.15)
     plt.scatter(X[y == 1, 0], X[y == 1, 1], label="Class #1", alpha=0.5, linewidth=0.15, c='y')
     plt.legend()
+    plt.title(l)
     return plt.show()
