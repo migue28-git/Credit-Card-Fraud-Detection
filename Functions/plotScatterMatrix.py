03ebd83ea7d7fab558bfbe8c728cfafe93002bed
Miguel Angel Nava Portillo <miguelangel.navaportillo@gmail.com>
2022-09-18 12:47:16 -0500

    Function for plotting variables relationships

---
+++ Functions/plotScatterMatrix.py
@@ -0,0 +1,18 @@
+def plotScatterMatrix(df, plotSize, textSize):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    df = df.select_dtypes(include =[np.number]) # keep only numerical columns
+    # Remove rows and columns that would lead to df being singular
+    df = df.dropna('columns')
+    df = df[[col for col in df if df[col].nunique() > 1]] # keep columns where there are more than 1 unique values
+    columnNames = list(df)
+    if len(columnNames) > 10: # reduce the number of columns for matrix inversion of kernel density plots
+        columnNames = columnNames[:10]
+    df = df[columnNames]
+    ax = pd.plotting.scatter_matrix(df, alpha=0.75, figsize=[plotSize, plotSize], diagonal='kde')
+    corrs = df.corr().values
+    for i, j in zip(*plt.np.triu_indices_from(ax, k = 1)):
+        ax[i, j].annotate('Corr. coef = %.3f' % corrs[i, j], (0.8, 0.2), xycoords='axes fraction', ha='center', va='center', size=textSize)
+    plt.suptitle('Scatter and Density Plot')
+    plt.show()
