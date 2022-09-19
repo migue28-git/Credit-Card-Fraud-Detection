015e79ffd5203df8a8f0eee6f7a3045a40306c15
Miguel Angel Nava Portillo <miguelangel.navaportillo@gmail.com>
2022-09-18 21:15:45 -0500

    Function for plotting a dataframe PCA transformation(classification class must be in last column)

--- Functions/plot_PCA_binary.py
+++ Functions/plot_PCA_binary.py
@@ -7,6 +7,7 @@ def plot_PCA_binary(df,t='Plot'):
     df_normalized = MinMaxScaler().fit_transform(df.copy())
     # Create a PCA instance with 2 components:
     pca_features = PCA(n_components=2).fit_transform(df_normalized[:, :-1])
+    plt.figure()
     mglearn.discrete_scatter(pca_features[:, 0], pca_features[:, 1], df_normalized[:, -1])
     plt.legend(['0', '1'])
     plt.xlabel('PCA1')
